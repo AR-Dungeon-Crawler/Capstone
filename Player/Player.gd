4ae1462e024c7cfe9184b01cf0179f1c100d78fd
Robertm8 <71862295+Robertm8@users.noreply.github.com>
2022-04-25 16:51:50 -0700

    Added Arrow Shooting
    Edited an arrow pixel sprite using pixlr.com's photo editor. Added it to the the shooting sequence for the Player in the game. Arrow fires on mouse up event. Eventually will add requirement to hold down mouse long enough or arrow won't fire. No hitboxes / collision. Arrow despawns after 2 seconds.

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
-# Load constants file
+# Load constants file and scenes
 onready var C = constants
+var Arrow = preload("res://Player/Arrow.tscn")
 
 var velocity = Vector2.ZERO
 
@@ -9,8 +10,7 @@ onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get(C.playback)
 
-# What does this do? This makes the location of player available globally
-# Ahh gotcha. Makes sense.
+
 func _ready():
 	C.player = self
 
@@ -26,19 +26,33 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength(C.down) - Input.get_action_strength(C.up)
 	input_vector = input_vector.normalized()
 	
-	var mouse_loc = get_viewport().get_mouse_position() - Vector2(C.width/2, C.height/2)
+	var raw_mouse_loc = get_viewport().get_mouse_position()  # absolute pixel location
+	var mouse_loc = raw_mouse_loc - Vector2(C.width/2, C.height/2)
 	update_animation_blends(mouse_loc)
 	
-	if Input.is_action_pressed("i_shoot"):
+	# Fire the arrow
+	if Input.is_action_just_released("i_shoot"):
+		var arrow = Arrow.instance()
+		get_parent().add_child(arrow)
+		arrow.dest = mouse_loc.normalized()
+		arrow.look_at(mouse_loc.normalized())  # rotates the sprite
+		arrow.position = position + mouse_loc.normalized() * arrow.offset
+	
+	# Load the arrow
+	elif Input.is_action_pressed("i_shoot"):
 		var current_node = animationState.get_current_node()
+		# Only play draw animation if not currently drawing (prevents unwanted animation loops)
 		if current_node == "Move" or current_node == "Idle":
 			animationState.travel("DrawBow")
+		# Come to a stop when starting to draw
 		velocity = velocity.move_toward(Vector2.ZERO, C.FRICTION * delta)
 	
+	# Move
 	elif input_vector != Vector2.ZERO:
 		animationState.travel("Move")
 		velocity = velocity.move_toward(input_vector * C.MAX_SPEED, C.ACCELERATION * delta)
 		
+	# Idle
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, C.FRICTION * delta)
@@ -47,11 +61,11 @@ func _physics_process(delta):
 		
 	
 func update_animation_blends(mouse_loc):
+	"""
+	Updates all the AnimationTree blends.
+	"""
 	animationTree.set(C.idleBlend, mouse_loc)
 	animationTree.set(C.moveBlend, mouse_loc)
 	animationTree.set(C.moveReverseBlend, mouse_loc)
 	animationTree.set(C.drawBowBlend, mouse_loc)
 	animationTree.set(C.holdBowBlend, mouse_loc)
-	
-	
-	
