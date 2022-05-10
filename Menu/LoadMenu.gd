51a6ebcd891a1bd806c5e3409a8ac66f195cfc8a
Jin Wu <jinwu281@gmail.com>
2022-05-09 20:27:20 -0700

    fixed merge conflicts

--- Menu/LoadMenu.gd
+++ Menu/LoadMenu.gd
@@ -12,24 +12,24 @@ func _ready():
 	#get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_2D, SceneTree.STRETCH_ASPECT_KEEP, map_size_px)
 	#OS.set_window_size(64 * Vector2(16, 9))
 	# TEST version
-#	var temp_path = ProjectSettings.globalize_path("res://dungeon.txt")
-#	var img2grid_path = ProjectSettings.globalize_path("res://PhotoToGrid.exe")
-#
-##	# BUILD version
-##	var temp_path = str(OS.get_executable_path().get_base_dir()) + "/dungeon.txt"
-##	var img2grid_path = str(OS.get_executable_path().get_base_dir()) + "/PhotoToGrid.exe"
-#
-#	var file = File.new()
-#	if file.file_exists(temp_path):
-#		var file_remove = Directory.new()
-#		file_remove.remove(temp_path)
-#
-#	OS.shell_open(img2grid_path)
-#
-#	var file2 = File.new()
-#
-#	while !file2.file_exists(temp_path):
-#		continue
+	var temp_path = ProjectSettings.globalize_path("res://dungeon.txt")
+	var img2grid_path = ProjectSettings.globalize_path("res://PhotoToGrid.exe")
+
+#	# BUILD version
+#	var temp_path = str(OS.get_executable_path().get_base_dir()) + "/dungeon.txt"
+#	var img2grid_path = str(OS.get_executable_path().get_base_dir()) + "/PhotoToGrid.exe"
+
+	var file = File.new()
+	if file.file_exists(temp_path):
+		var file_remove = Directory.new()
+		file_remove.remove(temp_path)
+
+	OS.shell_open(img2grid_path)
+
+	var file2 = File.new()
+	
+	while !file2.file_exists(temp_path):
+		continue
 	
 	yield(get_tree().create_timer(0.25), "timeout")
 	
--- World/Room.gd
+++ World/Room.gd
@@ -8,55 +8,56 @@ var enemyScene = load("res://Enemies/Bat.tscn")
 #export var perimeter_size := Vector2(1, 1)
 #onready var size := inner_size + 2 * perimeter_size
 
-#func setup() -> void:
+func setup() -> void:
+	return
 	# TEST version
-#	var temp_path = ProjectSettings.globalize_path("res://dungeon.txt")
-#	var img2grid_path = ProjectSettings.globalize_path("res://PhotoToGrid.exe")
-#
-##	# BUILD version
-##	var temp_path = str(OS.get_executable_path().get_base_dir()) + "/dungeon.txt"
-##	var img2grid_path = str(OS.get_executable_path().get_base_dir()) + "/PhotoToGrid.exe"
-#
-#	var file = File.new()
-#	file.open(temp_path, file.READ)
-#
-#	_tile_map.clear()
-#
-#	var x = 0
-#	var y = 0
-#
-#	while !file.eof_reached():
-#		var line = file.get_csv_line()
-#
-#		for item in line:
-#			if item == "#":
-#				_tile_map.set_cell(x, y, 1)
-#
-#			elif item == " " or "E" or "S":
-#				_tile_map.set_cell(x, y, 2)
-#
-#				if item == "E":
-#					var enemy = enemyScene.instance()
-#					enemy.position.y = y * 16
-#					enemy.position.x = x * 16
-#					add_child(enemy)
-#
-#				if item =="S":
-#					_player.set_global_position(Vector2((x * 16), (y * 16)))
-#
-#			x += 1
-#
-#		print(line)
-#		y += 1
-#		x = 0
-#	file.close()
-#	# Sets the game window size to twice the resolution of the world.
-#	#var map_size_px := size * _tile_map.cell_size
-#	#get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_2D, SceneTree.STRETCH_ASPECT_KEEP, map_size_px)
-#	#OS.set_window_size(1 * map_size_px)
-#
-#	var file_remove = Directory.new()
-#	file_remove.remove(temp_path)
-#
-#func _ready() -> void:
-#	setup()
+	var temp_path = ProjectSettings.globalize_path("res://dungeon.txt")
+	var img2grid_path = ProjectSettings.globalize_path("res://PhotoToGrid.exe")
+	
+#	# BUILD version
+#	var temp_path = str(OS.get_executable_path().get_base_dir()) + "/dungeon.txt"
+#	var img2grid_path = str(OS.get_executable_path().get_base_dir()) + "/PhotoToGrid.exe"
+
+	var file = File.new()
+	file.open(temp_path, file.READ)
+	
+	_tile_map.clear()
+	
+	var x = 0
+	var y = 0
+	
+	while !file.eof_reached():
+		var line = file.get_csv_line()
+		
+		for item in line:
+			if item == "#":
+				_tile_map.set_cell(x, y, 1)
+				
+			elif item == " " or "E" or "S":
+				_tile_map.set_cell(x, y, 2)
+				
+				if item == "E":
+					var enemy = enemyScene.instance()
+					enemy.position.y = y * 16
+					enemy.position.x = x * 16
+					add_child(enemy)
+					
+				if item =="S":
+					_player.set_global_position(Vector2((x * 16), (y * 16)))
+					
+			x += 1
+			
+		print(line)
+		y += 1
+		x = 0
+	file.close()
+	# Sets the game window size to twice the resolution of the world.
+	#var map_size_px := size * _tile_map.cell_size
+	#get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_2D, SceneTree.STRETCH_ASPECT_KEEP, map_size_px)
+	#OS.set_window_size(1 * map_size_px)
+	
+	var file_remove = Directory.new()
+	file_remove.remove(temp_path)
+
+func _ready() -> void:
+	setup()
