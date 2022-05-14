616721de50ec1203b7b25b1e7d276c212a2ab67c
Danny Chung <chungd87@gmail.com>
2022-05-13 23:58:08 -0700

    additional instructions
    additional instructions

--- Menu/Instructions2.gd
+++ Menu/Instructions2.gd
@@ -2,7 +2,3 @@ extends Control
 
 func _on_Back_pressed():
 	get_tree().change_scene("res://Menu/Menu.tscn")
-
-
-func _on_Mechanics_pressed():
-	get_tree().change_scene("res://Menu/Instructions2.tscn")
