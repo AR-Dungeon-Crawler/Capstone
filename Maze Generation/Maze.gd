d30204b26b9731edbee29da7b9babeee596a831a
Jin Wu <jinwu281@gmail.com>
2022-05-15 20:24:22 -0700

    updated bat starting positions so they don't start on walls

--- Maze Generation/Maze.gd
+++ Maze Generation/Maze.gd
@@ -12,6 +12,9 @@ var tile_size = 16  # tile size (in pixels)
 var width = 12  # width of map (in tiles)
 var height = 10  # height of map (in tiles)
 var rng = RandomNumberGenerator.new()
+var player_coordinates
+var bat_coordinates
+var bat_locations_array = []
 
 # get a reference to the map for convenience
 onready var Map = $TileMap
@@ -34,28 +37,36 @@ func check_neighbors(cell, unvisited):
 func make_maze():
 	var unvisited = []  # array of unvisited tiles
 	var stack = []
-	# fill the map with wall tiles
-	var player_coordinates = (Vector2(round(player.global_position[0] / 16), round(player.global_position[1] / 16)))
-	print(player_coordinates)
+	# fill the map with wall tiles initially 
 	Map.clear()
+	
+	# set the player and bat locations to be a walkable tile. This needs to be updated so 
+	# the player and bat can start on random locations 
+	player_coordinates = (Vector2(round(player.global_position[0] / 16), round(player.global_position[1] / 16)))
+	Map.set_cellv(player_coordinates, 2)
+	
+	for bat in get_tree().get_nodes_in_group("Enemy"):
+		bat_coordinates = (Vector2(round(bat.global_position[0] / 16), round(bat.global_position[1] / 16)))
+		bat_locations_array.append(bat_coordinates)
+		Map.set_cellv(bat_coordinates, 2)
+		
 	for x in range(width):
 		for y in range(height):
+			if Vector2(x, y) in bat_locations_array or Vector2(x, y) == player_coordinates:
+				continue
 			unvisited.append(Vector2(x, y))
 			Map.set_cellv(Vector2(x, y), 1)
-	
-	Map.set_cellv(player_coordinates, 2)
+		
 	var current = Vector2(0, 0)
 	unvisited.erase(current)
 	# execute recursive backtracker algorithm
 	while unvisited:
 		var neighbors = check_neighbors(current, unvisited)
 		if neighbors.size() > 0:
+			# pick a random neighbor cell and append to stack
 			var next = neighbors[randi() % neighbors.size()]
 			stack.append(current)
-			# remove walls from *both* cells
-			var dir = next - current
-			var current_walls = Map.get_cellv(current) - cell_walls[dir]
-			var next_walls = Map.get_cellv(next) - cell_walls[-dir]
+			# set current and next cell to be either a wall or walkable tile
 			Map.set_cellv(current, rng.randi_range(1, 2))
 			Map.set_cellv(next, rng.randi_range(1, 2))
 			current = next
@@ -64,6 +75,7 @@ func make_maze():
 			current = stack.pop_back()
 		yield(get_tree(), "idle_frame")
 	
+	# add additional layer of wall to the existing maze 
 	for x in range(-1, width + 1):
 		for y in range(-1, height + 1):
 			if x == -1 or x == width or y == -1 or y == height:
