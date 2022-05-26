e1e684e1e2979e00517ff34a20d66df1001d9aad
Danny Chung <chungd87@gmail.com>
2022-05-26 13:04:10 -0700

    wizardendmenufix
    wizardendmenufix

--- Menu/Scripts/EndGameWizard.gd
+++ Menu/Scripts/EndGameWizard.gd
@@ -12,8 +12,8 @@ func _on_PlayAgainWiz_pressed():
 func _on_PlayAgainWizWin_pressed():
 	get_tree().change_scene("res://World/PlayWizardRoom.tscn")
 
-#func _on_Random_Maze_pressed():
-#	get_tree().change_scene("res://Maze Generation/MazeWizard.tscn")
+func _on_Random_Maze_pressed():
+	get_tree().change_scene("res://Maze Generation/MazeWizard.tscn")
 
 func _on_MainMenuWinWiz_pressed():
 	get_tree().change_scene("res://Menu/Menu.tscn")
